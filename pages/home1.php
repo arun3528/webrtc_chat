214977c42ea156b3f352e6d2d60f17df31ee104d
arun3528 <arun3528@gmail.com>
2015-08-29 23:31:06 -0700

    chg: dev : added bootstrap

--- pages/home1.php
+++ pages/home1.php
@@ -3,24 +3,47 @@
 ?>
 <!DOCTYPE html>
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="../js/bootstrap.min.js"></script>
+
 	<link rel="stylesheet" type="text/css" href="../style/style.css">
 	<title>Chat Application Home</title>
 </head>
 <body>
-	<h2>Welcome <span style="color:green"><?php  echo $_SESSION['userName']; ?></span></h2>
-</br></br>
-  <span style="display:none" id='username'><?php  echo $_SESSION['userName']; ?></span>
-  <div class='answerCall' style="display:none">
-  <button id="receiveCall" name="receiveCall">Receive Call</button>
-  <button id="rejectCall" name="rejectCall">Reject Call</button>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+    
+      <a class="navbar-brand" href="#">
+        <label>webrtc</label> 
+           </a>
+      <label class="navbar-text pull-right" style="color:black">Welcome <small><?php  echo $_SESSION['userName']; ?></small></label>
+             
+    </div>
+  </div>
+</nav>
+<div class="container">
+	 <label id="username" style="display:none"><?php  echo $_SESSION['userName']; ?></label>
+  <div class='answerCall pull-right' style="display:none">
+  <button class="btn btn-primary" id="receiveCall" name="receiveCall">Receive Call</button>
+  <button class="btn btn-primary" id="rejectCall" name="rejectCall">Reject Call</button>
 	</div>
-	<input id="ChatText" name="ChatText"/>
-  <button class="btn btn-primary" onclick="connect()" id="dial">Dial</button>
+	<div class="form-inline">
+	<!-- TODO: Change the name --> 
+     	<input id="ChatText" type='text' name="ChatText" placeholder="UserId"/>
+      <button class="btn btn-primary" onclick="connect()" id="dial">Dial</button>
+  </div> 
   
-  
-<video id="remoteVideo" class="video-box col-xs-12  flip-horizontal" autoplay></video>
-<video id="localVideo" class="video-box col-xs-12  flip-horizontal" autoplay></video>
-
+  <video id="remoteVideo" class="video-box col-xs-12  flip-horizontal" autoplay></video>
+  <video id="localVideo" class="video-box col-xs-12  flip-horizontal" autoplay></video>
+</div>
 	<script src="//js.pusher.com/2.2/pusher.min.js"></script>
 	<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
 	<script type="text/javascript" src="../js/adapter.js"></script>
